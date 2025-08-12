c5ac43f0e706db303cb2436e7961cae5600ce9b4
MideJoy Akinleye <joyintech50@gmail.com>
2025-08-12 14:58:15 +0100

    JS update

---
+++ script.js
@@ -0,0 +1,18 @@
+const coin = document.querySelector(".head");
+const flipBtn = document.querySelector(".flipbtn");
+const resultText = document.querySelector(".result");
+
+function flipCoin() {
+    coin.classList.add("spin");
+
+    // SetTimeout is a built-in javascript API
+    setTimeout(() => {
+        coin.classList.remove("spin");
+        const isHeads = Math.random() < 0.5;
+        coin.src = isHeads ? "./resources/heads.svg" : "./resources/tails.svg";
+        resultText.textContent = isHeads ? "Heads" : "Tails";
+    }, 1000); // match animation duration, 1 sec = 1000ms
+}
+
+coin.addEventListener("click", flipCoin);
+flipBtn.addEventListener("click", flipCoin);
